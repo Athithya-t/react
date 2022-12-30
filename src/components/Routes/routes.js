52d0dfda571c297d18da6764d4c71fac04b7b9eb
Sathya Sivam <sathyasivamloganathan@gmail.com>
2022-12-30 07:22:35 +0530

    first commit

--- src/components/Routes/routes.js
+++ src/components/Routes/routes.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Route, Routes, Navigate } from "react-router-dom";
-import Home from '../../components/pages/Home.jsx';
+import { Route, Routes, Navigate, HashRouter } from "react-router-dom";
 
 const routes = () => {
     return(
+        <HashRouter>
         <Routes>
             <Route path='/' element={<Navigate to='/home'/>} />
             <Route path='/aboutUs '/>
 
         </Routes>
+        </HashRouter>
     );
 };
 
-export default Routes;
+export default routes;
